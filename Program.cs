9ce4fd22f4d77cc87a56ae68fc621f0d8f2078e7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-17 20:34:32 -0300

    a

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite: ");
+
+ Console.WriteLine(Console.ReadLine()!.ToUpper()
+  .Replace("A","4")
+  .Replace("E","3")
+  .Replace("I","1")
+  .Replace("L","1")
+  .Replace("O","0")
+  .Replace("T","7")
+  .Replace("S","5")
+  );
